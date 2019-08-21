61399e77c52a9d71004b4175a6eaf3e289130903
Gabriel Trettel <gabrielmtrettel@gmail.com>
2019-08-21 17:30:39 -0300

    SParser: 'leia' stmt finished

--- src/Parser.jl
+++ src/Parser.jl
@@ -5,11 +5,11 @@ iscoma(t::Token)  = t.id==PUNCTUATION && t.text==","
 isperiod(t::Token)= t.id==PUNCTUATION && t.text=="."
 
 mutable struct var_state
-    name        :: String
-    initialized :: Bool
-    type        :: String
-    value       :: String
-    function var_state(name, init=true, type="", value="")
+    name   :: String
+    init   :: Bool
+    type   :: String
+    value  :: String
+    function var_state(name, init=false, type="", value="")
         new(name, init, type, value)
     end
 end
@@ -23,6 +23,7 @@ function syntactic_parse(tokens::Tokens)
         t = next!(tokens)
         if t.id == IDENTIFIER
             bloco!(tokens, env)
+
         else
             error("AS: Parsing Error - Expecting name of program")
         end
@@ -62,6 +63,7 @@ function declare!(tokens::Tokens, env)
             for state in current_declared_vars
                 env[state.name] = state
             end
+            return
 
         else
             error("AS: $(t.text) Not an end of line.")
@@ -74,19 +76,52 @@ end
 
 
 function bloco!(tokens::Tokens, env)
-    #TODO onde fica o corpo do programa.
-    #     Tudo que vem entre [declare, fimprog)
-
     t = next!(tokens)
 
     if t.id==RESERVED_WORD && t.text=="declare"
         declare!(tokens, env)
+
+    elseif t.id==RESERVED_WORD && t.text=="leia"
+        cmd_leia!(tokens, env)
+
+    elseif t.id==RESERVED_WORD && t.text=="escreva"
+        cmd_escreva(tokens, env)
+
+    elseif t.id==IDENTIFIER
+        cmd_attr!(tokens, env)
+    end
+
+    if t.text!="fimprog"
+        bloco!(tokens, env)
     end
 end
 
 
 function cmd_leia!(tokens::Tokens, env)
-    #TODO leia(var).
+    @show t = next!(tokens)
+
+    if t.id==PUNCTUATION && t.text=="("
+        t = next!(tokens)
+        if t.id==IDENTIFIER
+            if haskey(env, t.text)
+                env[t.text].init = true
+            else
+                error("AS: Trying to read from indeclared var $(t.text)")
+            end # env
+
+        else # id
+            error("AS: Values inside 'leia' command must be a variable, not $(t.text)")
+        end # id
+        t = next!(tokens)
+        if t.id!=PUNCTUATION && t.text==")"
+            error("AS: Missing closing parenthesis in 'leia' stmt")
+        end # )
+
+        t = next!(tokens)
+        if !isperiod(t) error("AS: Missing period") end
+    else # (
+        error("AS: Missing open parenthesis in 'leia' stmt")
+    end # (
 end
 
 
@@ -104,5 +139,5 @@ end
 
 function par_expr!(tokens::Tokens, env)
     #TODO
-
+    1
 end
