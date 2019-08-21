5c931e47d629848e2a751cc5fe1cae1d71ec5359
Gabriel Trettel <gabrielmtrettel@gmail.com>
2019-08-20 22:30:49 -0300

    Token: fixing some bugs

--- src/Token.jl
+++ src/Token.jl
@@ -1,16 +1,3 @@
-module TokenDefinition
-
-export Token,
-       TokenIDS,
-       IDENTIFIER,
-       ASSIGN,
-       INT_NUMBER,
-       FLOAT_NUMBER,
-       OPERATOR,
-       RESERVED_WORD,
-       PUNCTUATION,
-       INVALID
-
 @enum TokenIDS begin
    IDENTIFIER     = 0
    ASSIGN         = 1
@@ -21,37 +8,41 @@ export Token,
    PUNCTUATION    = 6
    INVALID        = 7
    EOF            = -1
+   TYPE           = 10
 end # enum
 
 
 mutable struct Token
     id   :: TokenIDS
     text :: String
-    function Token(id=TokenIDS.INVALID, txt="")
+    function Token(id=INVALID, txt="")
         new(id, txt)
     end # function
 end # struct
 
 
 mutable struct Tokens
-   tokens :: Vector{Token}
+   tokens :: Array{Token}
    pos    :: Integer
    total  :: Integer
-   function Tokens(tks::Vector{Token})
+   function Tokens(tks::Array{Token})
       new(tks, 1, length(tks))
    end
 end
 
-function next_token(t::Tokens)
-   if t.total >= pos return TokenIDS.EOF end
+function next_token!(t::Tokens)
+   v = t.tokens[ t.pos ]
+   printstyled(v; color=:red)
 
+   if t.pos == t.total
+      return t.tokens[ t.pos ]
+   end
    t.pos += 1
-   return tokes[pos]
+
+   return v
 end
 
 # Base.show is invoked in Atom and @show
 Base.show(io::IO, t::Token) = println(io, "`$(t.text)` is $(t.id)")
 # Base.println is invoked in println call
 Base.println(t::Token) = println("`$(t.text)` is $(t.id)")
-
-end # module
