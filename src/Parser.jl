9410c4c0ed7ff1d4a8c5069f37a07d5e714841a1
Gabriel Trettel <gabrielmtrettel@gmail.com>
2019-08-20 22:30:06 -0300

    SParser: fixing bugs in 'declare' parser

--- src/Parser.jl
+++ src/Parser.jl
@@ -1,20 +1,22 @@
-module Parser
 
+# include("Token.jl")
+# using .TokenDefinition
+# using .TokenDefinition : Token, TokenIDS, IDENTIFIER, ASSIGN, INT_NUMBER, FLOAT_NUMBER,OPERATOR,RESERVED_WORD,PUNCTUATION,INVALID,Tokens
+# include("Portran.jl")
+
+# export syntactic_parse
 include("Token.jl")
-using .TokenDefinition
 
-export syntactic_parse
-
-iscolon(t::Token) = t.id==TokensIDS.PUNCTUATION && t.text==":"
-iscoma(t::Token)  = t.id==TokensIDS.PUNCTUATION && t.text==","
-isperiod(t::Token)= t.id==TokensIDS.PUNCTUATION && t.text=="."
+iscolon(t::Token) = t.id==PUNCTUATION && t.text==":"
+iscoma(t::Token)  = t.id==PUNCTUATION && t.text==","
+isperiod(t::Token)= t.id==PUNCTUATION && t.text=="."
 
 mutable struct var_state
     name        :: String
     initialized :: Bool
     type        :: String
     value       :: String
-    function state(name, init=true, type="", value="")
+    function var_state(name, init=true, type="", value="")
         new(name, init, type, value)
     end
 end
@@ -23,10 +25,10 @@ end
 function syntactic_parse(tokens::Tokens)
     env = Dict()
 
-    t = next_token(tokens)
-    if t.id == TokenIDS.RESERVERD_WORD && t.text == "programa"
-        t = next_token(tokens)
-        if t.id == TokenIDS.ID
+    t = next_token!(tokens)
+    if t.id == RESERVED_WORD && t.text == "programa"
+        t = next_token!(tokens)
+        if t.id == RESERVED_WORD && t.text == "declare"
             declare!(tokens, env)
         else
             error("AS: Parsing Error - Expecting ID")
@@ -35,8 +37,8 @@ function syntactic_parse(tokens::Tokens)
         error("AS: Parsing Error - Expecting PROGRAMA")
     end
 
-    t = next_token(tokens)
-    if t.id != TokenIDS.EOF
+    t = next_token!(tokens)
+    if t.id != EOF
         error("AS: Program finished but file has content")
     end
 end
@@ -44,37 +46,31 @@ end
 
 
 function declare!(tokens::Tokens, env)
-    t = next_token(tokens)
-    if t.id == TokenIDS.RESERVED_WORD && t.text == "declare"
-        t = next_token(tokens)
-        current_declared_vars = []
+    t = next_token!(tokens)
+    current_declared_vars = []
 
-        while t.id == TokensIDS.ID || iscoma(t)
-            push!(current_declared_vars, var_state(t.text))
-            t = next_token(tokens)
-        end
+    while t.id == IDENTIFIER || iscoma(t)
+        push!(current_declared_vars, var_state(t.text))
+        t = next_token!(tokens)
+    end
 
-        if iscolon(t)
-            t = next_token(tokens)
-            if t.id == TokensIDS.TYPE
-                map(x->x.type=t.text, current_declared_vars)
-            else
-                error("AS: $(t.text) is not a correct TYPE")
-            end
-
-            t = next_token(tokens)
-            if isperiod(t)
-                map!(x->push!(env, x.name=>x), current_declared_vars)
-                bloco(tokens, env)
-            else
-                error("AS: $(t.text) Not an end of line.")
-            end
+    if iscolon(t)
+        t = next_token!(tokens)
+        if t.id == TYPE
+            map(x->x.type=t.text, current_declared_vars)
         else
-            error("AS: $(t.text) Not a correct in DECLARE stmt")
+            error("AS: $(t.text) is not a correct TYPE")
         end
 
+        t = next_token!(tokens)
+        if isperiod(t)
+            map!(x->push!(env, x.name=>x), current_declared_vars)
+            bloco(tokens, env)
+        else
+            error("AS: $(t.text) Not an end of line.")
+        end
     else
-        error("AS: Not an valid DECLARE stmt")
+        error("AS: $(t.text) Not correct DECLARE stmt")
     end
 end
 
@@ -84,5 +80,3 @@ function bloco(tokens::Tokens, env)
     #TODO
     1
 end
-
-end # module
